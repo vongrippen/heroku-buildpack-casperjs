e2a0059014eb503c61ad7c5889920efac818c6e1
leesei <leesei@gmail.com>
2013-03-07 01:53:06 +0800

    fixed PATH
     Changes to be committed:
    	modified:   bin/casperjs.sh

--- bin/casperjs.sh
+++ bin/casperjs.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Add phantomjs/casperjs paths ..."
-PATH="${PATH}:/app/vendor/phantomjs/bin:vendor/casperjs/bin"
-LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:app/vendor/phantomjs/lib"
+PATH="${PATH}:/app/vendor/phantomjs/bin:/app/vendor/casperjs/bin"
+LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/app/vendor/phantomjs/lib"
 
